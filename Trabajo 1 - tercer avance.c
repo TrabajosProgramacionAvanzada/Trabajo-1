bd4b613d564738d2ad093b9e81cec951999e6997
IgnacioSanhueza <ignacio.sanhueza@usach.cl>
2019-09-13 02:30:38 -0300

    Actualización final
    falta reformatear el documento pero ya está re que te güeno

--- Trabajo 1 - tercer avance.c
+++ Trabajo 1 - tercer avance.c
@@ -57,7 +57,7 @@ void display(node **head) {
   if (temp->coef > 0) { // Si el coeficiente es mayor que cero
       printf("%ld x^%ld ", temp->coef, temp->grd); // Imprime con un signo mas.
     } else if (temp->coef < 0) {                    // Sino
-      printf("- %ld x^%ld ", temp->coef,
+      printf("- %ld x^%ld ", temp->coef * -1,
              temp->grd); // Imprime con un signo menos.
     }
   temp = temp->next;
